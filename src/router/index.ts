46aa6a6fd1c52edebb5bd5224034ba52f89cd3b1
eccwill <wchango@estud.tesa.edu.ec>
2026-02-10 17:18:50 -0500

    todo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,39 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import BaseLayout from '@/layouts/BaseLayout.vue';
+import { useUserStore } from '@/stores/user';
+
+const Login = () => import('@/views/Login.vue');
+const Registro = () => import('@/views/Registro.vue');
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/seccion'
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/registro',
+    name: 'Registro',
+    component: Registro,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/seccion',
+    name: 'Seccion',  
+    component: BaseLayout,
+    meta: {
+      requiresAuth: true
+    },    
   }
 ]
 
@@ -35,5 +41,18 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore();
+  const isAuthenticated = !!userStore.token;
+  if(to.meta.requiresAuth && !isAuthenticated) {
+    next('/login');
+  } else if(isAuthenticated && !to.meta.requiresAuth) {
+    next('/seccion');
+  }else {
+    next();
+  }
+});
+
 
 export default router
